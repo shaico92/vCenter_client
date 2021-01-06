a52d3911bc8ca898279cbe5824ff384887be06e8
Shai Cohen <Shai.c@Hartech.local>
2021-01-06 17:30:34 +0200

    adding focus on ssh form and fixing layout

--- src/sshEnableForm/sshEnableForm.js
+++ src/sshEnableForm/sshEnableForm.js
@@ -1,4 +1,4 @@
-import React,{useEffect,useState} from 'react'
+import React,{useEffect,useState,useRef} from 'react'
 
 import './sshEnableForm.css'
 
@@ -7,54 +7,67 @@ const EnableSSH = ({enableSSH})=>{
     const [Username, setUsername] = useState(null);
     const [Password, setPassword] = useState(null);
     const [IP, setIP] = useState(null);
-
+    const formSSH= useRef(null);
     useEffect(() => {
-        
+      if (formSSH.current) {
+        //formSSH.current.focus()
+        //formSSH.current.children[0].focus();
+        formSSH.current.children[0].children[0].focus()
+      }
     }, [open]);
 
 const closeOpenForm=()=>{
 
     if (!open||open===false) {
         setopen(true);
+      //  ipInput.current.focus()
     } else {
         setopen(false);
     }
+    
 }
 
 
-const form = <div className="sshForm">
-<input
+const form = <div ref={formSSH} className="sshForm">
+        <div  className="sshFormChild">
+        <input
             
             onChange={(event)=>setIP(event.target.value)}
             type="text"
             max="15"
             placeholder="Please enter ip"
           ></input>
-          <input
+          </div>
+          <div className="sshFormChild"> <input
           onChange={(event)=>setUsername(event.target.value)}
             type="text"
             placeholder="Please enter username"
-          ></input>
-          <input
+          ></input></div>
+          <div className="sshFormChild">  <input
           onChange={(event)=>setPassword(event.target.value)}
           placeholder="Please enter password"
             type="password"
             
-          ></input>
-          <button className="enableFormBtn"
+          ></input></div>
+          <div className="sshFormChild"> <button className="enableFormBtn"
             onClick={()=>enableSSH({ip: IP,username: Username,pass : Password})}
           >
             EnableSSH
-          </button>
+          </button></div>
+         
+        
+         
 
 </div>;
 
     return (
         <div >
-            <div className="enableFormBtn" onClick={closeOpenForm}>enable ssh</div>
+            <div className="enableFormBtn" onClick={closeOpenForm}>EnableSSH </div>
             {open&&open===true ? 
-            form:
-            null}
+            form:null
+            
+            
+            }
         </div>
     )
 }
